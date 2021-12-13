42a508f4899de9c72073d7e2de916569997fa596
clearhost-cmd <91465315+clearhost-cmd@users.noreply.github.com>
2021-12-13 10:39:12 +0000

    Button Update

--- components/button/base.vue
+++ components/button/base.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="submit" class="font-semibold mt-5 px-3 py-1.5 text-[10px] bg-gray-800 rounded-full text-white">
+  <button type="submit" class="font-semibold mt-5 px-3 py-1.5 text-[10px] bg-red-800 rounded-full text-white">
     {{ action }}
   </button>
 </template>
